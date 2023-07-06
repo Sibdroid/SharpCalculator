5238ca8b423a478df7be96fe3d96b7c413cd66c4
Sibdroid <ryabovglebrg@gmail.com>
2023-07-06 16:37:26 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.NumButton7 = new System.Windows.Forms.Button();
+			this.NumButton4 = new System.Windows.Forms.Button();
+			this.NumButton5 = new System.Windows.Forms.Button();
+			this.NumButton6 = new System.Windows.Forms.Button();
+			this.NumButton9 = new System.Windows.Forms.Button();
+			this.NumButton8 = new System.Windows.Forms.Button();
+			this.NumButton3 = new System.Windows.Forms.Button();
+			this.NumButton2 = new System.Windows.Forms.Button();
+			this.NumButton1 = new System.Windows.Forms.Button();
+			this.ResultLabel = new System.Windows.Forms.Label();
+			this.NumButton0 = new System.Windows.Forms.Button();
+			this.ClearButton = new System.Windows.Forms.Button();
+			this.ResultButton = new System.Windows.Forms.Button();
+			this.AddButton = new System.Windows.Forms.Button();
+			this.SubButton = new System.Windows.Forms.Button();
+			this.MulButton = new System.Windows.Forms.Button();
+			this.DivButton = new System.Windows.Forms.Button();
+			this.LeftBracketButton = new System.Windows.Forms.Button();
+			this.RightBracketButton = new System.Windows.Forms.Button();
+			this.EraseButton = new System.Windows.Forms.Button();
+			this.PreviewLabel = new System.Windows.Forms.Label();
+			this.PointButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// NumButton7
+			// 
+			this.NumButton7.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.NumButton7.Location = new System.Drawing.Point(0, 224);
+			this.NumButton7.Name = "NumButton7";
+			this.NumButton7.Size = new System.Drawing.Size(75, 75);
+			this.NumButton7.TabIndex = 0;
+			this.NumButton7.Text = "7";
+			this.NumButton7.UseVisualStyleBackColor = true;
+			// 
+			// NumButton4
+			// 
+			this.NumButton4.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.NumButton4.Location = new System.Drawing.Point(0, 304);
+			this.NumButton4.Name = "NumButton4";
+			this.NumButton4.Size = new System.Drawing.Size(75, 75);
+			this.NumButton4.TabIndex = 3;
+			this.NumButton4.Text = "4";
+			this.NumButton4.UseVisualStyleBackColor = true;
+			// 
+			// NumButton5
+			// 
+			this.NumButton5.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.NumButton5.Location = new System.Drawing.Point(80, 304);
+			this.NumButton5.Name = "NumButton5";
+			this.NumButton5.Size = new System.Drawing.Size(75, 75);
+			this.NumButton5.TabIndex = 4;
+			this.NumButton5.Text = "5";
+			this.NumButton5.UseVisualStyleBackColor = true;
+			// 
+			// NumButton6
+			// 
+			this.NumButton6.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.NumButton6.Location = new System.Drawing.Point(160, 304);
+			this.NumButton6.Name = "NumButton6";
+			this.NumButton6.Size = new System.Drawing.Size(75, 75);
+			this.NumButton6.TabIndex = 5;
+			this.NumButton6.Text = "6";
+			this.NumButton6.UseVisualStyleBackColor = true;
+			// 
+			// NumButton9
+			// 
+			this.NumButton9.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.NumButton9.Location = new System.Drawing.Point(160, 223);
+			this.NumButton9.Name = "NumButton9";
+			this.NumButton9.Size = new System.Drawing.Size(75, 75);
+			this.NumButton9.TabIndex = 7;
+			this.NumButton9.Text = "9";
+			this.NumButton9.UseVisualStyleBackColor = true;
+			// 
+			// NumButton8
+			// 
+			this.NumButton8.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.NumButton8.Location = new System.Drawing.Point(80, 223);
+			this.NumButton8.Name = "NumButton8";
+			this.NumButton8.Size = new System.Drawing.Size(75, 75);
+			this.NumButton8.TabIndex = 6;
+			this.NumButton8.Text = "8";
+			this.NumButton8.UseVisualStyleBackColor = true;
+			// 
+			// NumButton3
+			// 
+			this.NumButton3.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.NumButton3.Location = new System.Drawing.Point(160, 384);
+			this.NumButton3.Name = "NumButton3";
+			this.NumButton3.Size = new System.Drawing.Size(75, 75);
+			this.NumButton3.TabIndex = 10;
+			this.NumButton3.Text = "3";
+			this.NumButton3.UseVisualStyleBackColor = true;
+			// 
+			// NumButton2
+			// 
+			this.NumButton2.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.NumButton2.Location = new System.Drawing.Point(80, 384);
+			this.NumButton2.Name = "NumButton2";
+			this.NumButton2.Size = new System.Drawing.Size(75, 75);
+			this.NumButton2.TabIndex = 9;
+			this.NumButton2.Text = "2";
+			this.NumButton2.UseVisualStyleBackColor = true;
+			// 
+			// NumButton1
+			// 
+			this.NumButton1.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.NumButton1.Location = new System.Drawing.Point(0, 384);
+			this.NumButton1.Name = "NumButton1";
+			this.NumButton1.Size = new System.Drawing.Size(75, 75);
+			this.NumButton1.TabIndex = 8;
+			this.NumButton1.Text = "1";
+			this.NumButton1.UseVisualStyleBackColor = true;
+			// 
+			// ResultLabel
+			// 
+			this.ResultLabel.AutoEllipsis = true;
+			this.ResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.ResultLabel.Font = new System.Drawing.Font("Roboto", 40F);
+			this.ResultLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.ResultLabel.Location = new System.Drawing.Point(0, 52);
+			this.ResultLabel.Name = "ResultLabel";
+			this.ResultLabel.Size = new System.Drawing.Size(316, 85);
+			this.ResultLabel.TabIndex = 11;
+			this.ResultLabel.Text = "0";
+			this.ResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// NumButton0
+			// 
+			this.NumButton0.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.NumButton0.Location = new System.Drawing.Point(80, 465);
+			this.NumButton0.Name = "NumButton0";
+			this.NumButton0.Size = new System.Drawing.Size(75, 75);
+			this.NumButton0.TabIndex = 12;
+			this.NumButton0.Text = "0";
+			this.NumButton0.UseVisualStyleBackColor = true;
+			// 
+			// ClearButton
+			// 
+			this.ClearButton.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.ClearButton.Location = new System.Drawing.Point(0, 142);
+			this.ClearButton.Name = "ClearButton";
+			this.ClearButton.Size = new System.Drawing.Size(75, 75);
+			this.ClearButton.TabIndex = 13;
+			this.ClearButton.Text = "C";
+			this.ClearButton.UseVisualStyleBackColor = true;
+			this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+			// 
+			// ResultButton
+			// 
+			this.ResultButton.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.ResultButton.Location = new System.Drawing.Point(241, 465);
+			this.ResultButton.Name = "ResultButton";
+			this.ResultButton.Size = new System.Drawing.Size(75, 75);
+			this.ResultButton.TabIndex = 15;
+			this.ResultButton.Text = "=";
+			this.ResultButton.UseVisualStyleBackColor = true;
+			this.ResultButton.Click += new System.EventHandler(this.ResultButton_Click);
+			// 
+			// AddButton
+			// 
+			this.AddButton.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.AddButton.Location = new System.Drawing.Point(241, 384);
+			this.AddButton.Name = "AddButton";
+			this.AddButton.Size = new System.Drawing.Size(75, 75);
+			this.AddButton.TabIndex = 16;
+			this.AddButton.Text = "+";
+			this.AddButton.UseVisualStyleBackColor = true;
+			// 
+			// SubButton
+			// 
+			this.SubButton.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.SubButton.Location = new System.Drawing.Point(241, 303);
+			this.SubButton.Name = "SubButton";
+			this.SubButton.Size = new System.Drawing.Size(75, 75);
+			this.SubButton.TabIndex = 17;
+			this.SubButton.Text = "-";
+			this.SubButton.UseVisualStyleBackColor = true;
+			// 
+			// MulButton
+			// 
+			this.MulButton.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.MulButton.Location = new System.Drawing.Point(241, 222);
+			this.MulButton.Name = "MulButton";
+			this.MulButton.Size = new System.Drawing.Size(75, 75);
+			this.MulButton.TabIndex = 18;
+			this.MulButton.Text = "*";
+			this.MulButton.UseVisualStyleBackColor = true;
+			// 
+			// DivButton
+			// 
+			this.DivButton.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.DivButton.Location = new System.Drawing.Point(241, 142);
+			this.DivButton.Name = "DivButton";
+			this.DivButton.Size = new System.Drawing.Size(75, 75);
+			this.DivButton.TabIndex = 19;
+			this.DivButton.Text = "/";
+			this.DivButton.UseVisualStyleBackColor = true;
+			// 
+			// LeftBracketButton
+			// 
+			this.LeftBracketButton.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.LeftBracketButton.Location = new System.Drawing.Point(0, 465);
+			this.LeftBracketButton.Name = "LeftBracketButton";
+			this.LeftBracketButton.Size = new System.Drawing.Size(75, 75);
+			this.LeftBracketButton.TabIndex = 20;
+			this.LeftBracketButton.Text = "(";
+			this.LeftBracketButton.UseVisualStyleBackColor = true;
+			// 
+			// RightBracketButton
+			// 
+			this.RightBracketButton.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.RightBracketButton.Location = new System.Drawing.Point(160, 465);
+			this.RightBracketButton.Name = "RightBracketButton";
+			this.RightBracketButton.Size = new System.Drawing.Size(75, 75);
+			this.RightBracketButton.TabIndex = 21;
+			this.RightBracketButton.Text = ")";
+			this.RightBracketButton.UseVisualStyleBackColor = true;
+			// 
+			// EraseButton
+			// 
+			this.EraseButton.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.EraseButton.Location = new System.Drawing.Point(80, 142);
+			this.EraseButton.Name = "EraseButton";
+			this.EraseButton.Size = new System.Drawing.Size(75, 75);
+			this.EraseButton.TabIndex = 22;
+			this.EraseButton.Text = "⌫";
+			this.EraseButton.UseVisualStyleBackColor = true;
+			this.EraseButton.Click += new System.EventHandler(this.EraseButton_Click);
+			// 
+			// PreviewLabel
+			// 
+			this.PreviewLabel.AutoEllipsis = true;
+			this.PreviewLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.PreviewLabel.Font = new System.Drawing.Font("Roboto", 20F);
+			this.PreviewLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.PreviewLabel.Location = new System.Drawing.Point(0, -2);
+			this.PreviewLabel.Name = "PreviewLabel";
+			this.PreviewLabel.Size = new System.Drawing.Size(316, 50);
+			this.PreviewLabel.TabIndex = 23;
+			this.PreviewLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// PointButton
+			// 
+			this.PointButton.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.PointButton.Location = new System.Drawing.Point(160, 142);
+			this.PointButton.Name = "PointButton";
+			this.PointButton.Size = new System.Drawing.Size(75, 75);
+			this.PointButton.TabIndex = 24;
+			this.PointButton.Text = ".";
+			this.PointButton.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(317, 541);
+			this.Controls.Add(this.PointButton);
+			this.Controls.Add(this.PreviewLabel);
+			this.Controls.Add(this.EraseButton);
+			this.Controls.Add(this.RightBracketButton);
+			this.Controls.Add(this.LeftBracketButton);
+			this.Controls.Add(this.DivButton);
+			this.Controls.Add(this.MulButton);
+			this.Controls.Add(this.SubButton);
+			this.Controls.Add(this.AddButton);
+			this.Controls.Add(this.ResultButton);
+			this.Controls.Add(this.ClearButton);
+			this.Controls.Add(this.NumButton0);
+			this.Controls.Add(this.ResultLabel);
+			this.Controls.Add(this.NumButton3);
+			this.Controls.Add(this.NumButton2);
+			this.Controls.Add(this.NumButton1);
+			this.Controls.Add(this.NumButton9);
+			this.Controls.Add(this.NumButton8);
+			this.Controls.Add(this.NumButton6);
+			this.Controls.Add(this.NumButton5);
+			this.Controls.Add(this.NumButton4);
+			this.Controls.Add(this.NumButton7);
+			this.Name = "Form1";
+			this.Text = "  ";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button NumButton7;
+		private System.Windows.Forms.Button NumButton4;
+		private System.Windows.Forms.Button NumButton5;
+		private System.Windows.Forms.Button NumButton6;
+		private System.Windows.Forms.Button NumButton9;
+		private System.Windows.Forms.Button NumButton8;
+		private System.Windows.Forms.Button NumButton3;
+		private System.Windows.Forms.Button NumButton2;
+		private System.Windows.Forms.Button NumButton1;
+		private System.Windows.Forms.Label ResultLabel;
+		private System.Windows.Forms.Button NumButton0;
+		private System.Windows.Forms.Button ClearButton;
+		private System.Windows.Forms.Button ResultButton;
+		private System.Windows.Forms.Button AddButton;
+		private System.Windows.Forms.Button SubButton;
+		private System.Windows.Forms.Button MulButton;
+		private System.Windows.Forms.Button DivButton;
+		private System.Windows.Forms.Button LeftBracketButton;
+		private System.Windows.Forms.Button RightBracketButton;
+		private System.Windows.Forms.Button EraseButton;
+		private System.Windows.Forms.Label PreviewLabel;
+		private System.Windows.Forms.Button PointButton;
+	}
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,236 @@
+﻿using NCalc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		bool is_changed = false;
+		public Form1()
+		{
+			InitializeComponent();
+			this.KeyPreview = true;
+			this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+			NumButton0.Click += buttonClick;
+			NumButton1.Click += buttonClick;
+			NumButton2.Click += buttonClick;
+			NumButton3.Click += buttonClick;
+			NumButton4.Click += buttonClick;
+			NumButton5.Click += buttonClick;
+			NumButton6.Click += buttonClick;
+			NumButton7.Click += buttonClick;
+			NumButton8.Click += buttonClick;
+			NumButton9.Click += buttonClick;
+			AddButton.Click += buttonClick;
+			SubButton.Click += buttonClick;
+			MulButton.Click += buttonClick;
+			DivButton.Click += buttonClick;
+			PointButton.Click += buttonClick;
+			LeftBracketButton.Click += buttonClick;
+			RightBracketButton.Click += buttonClick;
+		}
+
+		private void buttonClick(object sender, EventArgs e)
+		{
+			if (!is_changed)
+			{
+				ResultLabel.Text = "";
+				is_changed = true;
+			}
+			System.Windows.Forms.Button button = sender as System.Windows.Forms.Button;
+			string text = button.Text;
+			ResultLabel.Text += text;
+			Tools.resizeFont(ResultLabel);
+		}
+		private void ClearButton_Click(object sender, EventArgs e)
+		{
+			Tools.clear(ResultLabel, PreviewLabel);
+			is_changed = false;
+		}
+
+		private void ResultButton_Click(object sender, EventArgs e)
+		{
+			Tools.calculateResult(ResultLabel, PreviewLabel);
+		}
+
+		private void EraseButton_Click(object sender, EventArgs e)
+		{
+			Tools.erase(ResultLabel);
+			if (ResultLabel.Text.Length == 0)
+			{
+				ResultLabel.Text = "0";
+				is_changed = false;
+			}
+		}
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			string text = e.KeyCode.ToString();
+			string value = "";
+			bool overwriteChange = false;
+			if (Control.ModifierKeys == Keys.Shift)
+			{
+				switch (text)
+				{
+					case "D9":
+						value = "(";
+						break;
+					case "D0":
+						value = ")";
+						break;
+					case "Oemplus":
+						value = "+";
+						break;
+					case "OemMinus":
+						value = "-";
+						break;
+					case "OemQuestion":
+						value = ".";
+						break;
+					case "OemPeriod":
+						value = ".";
+						break;
+					default:
+						overwriteChange = true;
+						break;
+				}
+			}
+			else
+			{
+				switch (text)
+				{
+					case "Add":
+						value = "+";
+						break;
+					case "Subtract":
+						value = "-";
+					    break;
+					case "Multiply":
+						value = "*";
+						break;
+					case "Divide":
+						value = "/";
+						break;
+					case "Decimal":
+						value = ".";
+						break;
+					case "OemMinus":
+						value = "-";
+						break;
+					case "OemQuestion":
+						value = ".";
+						break;
+					case "OemPeriod":
+						value = ".";
+						break;
+					case "Back":
+						Tools.erase(ResultLabel);
+						if (ResultLabel.Text.Length == 0)
+						{
+							ResultLabel.Text = "0";
+							is_changed = false;
+							overwriteChange = true;
+						}
+						break;
+					case "C":
+						Tools.clear(ResultLabel, PreviewLabel);
+						is_changed = false;
+						overwriteChange = true;
+						break;
+					case "Oemplus":
+						Tools.calculateResult(ResultLabel, PreviewLabel);
+						overwriteChange = true;
+						break;
+					default:
+						if (text.Length == 2 && text[0].ToString() == "D")
+						{
+							value = text[1].ToString();
+						}	
+						else if (text.Length == 7 && text.Contains("NumPad"))
+						{
+							value = text[text.Length - 1].ToString();
+						}
+						else
+						{
+							overwriteChange = true;
+							Console.WriteLine(text);
+						}
+						break;
+				}
+			}
+			if (!is_changed && !overwriteChange)
+			{
+				ResultLabel.Text = "";
+				is_changed = true;
+			}
+			ResultLabel.Text += value;
+			Tools.resizeFont(ResultLabel);
+		}
+	}
+	public class Tools
+	{
+		public static void resizeFont(Label label)
+		{
+			var size = default(SizeF);
+			do
+			{
+				using (var font = new Font(label.Font.Name, label.Font.SizeInPoints))
+				{
+					size = TextRenderer.MeasureText(label.Text, font);
+					if (size.Width > label.Width)
+					{
+						label.Font = new Font(font.Name, font.SizeInPoints - 1f);
+					}
+				}
+			} while (size.Width > label.Width);
+		}
+		public static void erase(Label label)
+		{
+			label.Text = label.Text.Remove(label.Text.Length - 1);
+			resizeFont(label);
+		}
+		public static void clear(Label label, Label preview)
+		{
+			label.Text = "0";
+			preview.Text = "";
+			resizeFont(label);
+		}
+		public static void calculateResult(Label label, Label preview)
+		{
+			label.Text = label.Text.Replace(",", ".");
+			Expression expression = new Expression(label.Text);
+			preview.Text = label.Text;
+			try
+			{
+				label.Text = Convert.ToString(expression.Evaluate());
+				label.Text = label.Text.Replace(",", ".");
+				foreach (char c in label.Text)
+				{
+					if (!"0123456789.-".Contains(c))
+					{
+						label.Text = "#";
+						break;
+					}
+				}
+			}
+			catch (EvaluationException e)
+			{
+				label.Text = "#";
+			}
+			resizeFont(label);
+			resizeFont(preview);
+		}
+	}
+}  
+
